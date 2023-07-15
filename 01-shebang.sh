dad9028f4b3bc9312166d8fd0963905d204d007e
guna <gunadeeprakeshm@gmail.com>
2023-07-15 13:26:51 +0530

    Auto push

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 echo Welcome to Shell-scripting.
 #echo--> This command is used for printing the required text. In this case the above command just prints the text  Welcome to shell-scripting.
+ echo line1
+ ecHo line2
+ 
