2e186050980e8711698b254a31fa713a209289be
guna <gunadeeprakeshm@gmail.com>
2023-07-16 15:30:58 +0530

    AutoPush

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 ### There are some variables, whose values gets changed withrespect to time. These are called as "Dynamic variables".
 ### These should not be hardcoded as their data/value gets changed too often. So,instead we should softcode them using expressions to dynamically represent the results.
+
 # For example,
 echo -e "\e[32m Today's date is 22 Nov 2021 \e[0m"
+# In the above line, we have hardcoded the value...So it prints the same date for every day, which is factually incorrect.
+
+# So, now we'll softcode it and create an expression to dynamically query the info from system and can print the necessary data.
+echo -e "\e[36m Today's date is $(date +%D) \e[0m"
