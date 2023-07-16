4b2443903016293865392b954261183732d61f46
guna <gunadeeprakeshm@gmail.com>
2023-07-16 13:40:44 +0530

    added variables file

--- 04-variables.sh
+++ 04-variables.sh
@@ -20,4 +20,6 @@ k=14
 j=
 FOLDER_TO_DELETE=
 ### If you try to print a variable that is not declared ahead, Bash will consider that as a null, instead of throwing you an error like others do, 
-### For ex:-  "rm -rf /data/${FOLDER_TO_DELETE}/ = rm -rf /data" --> The meaning of this command is..it deletes the complete directory or the file under the data directory, as we have not declared that specific variable. So we should be in our senses of what we're executing.
+### For ex:-  "rm -rf /data/${FOLDER_TO_DELETE}/ = rm -rf /data" --> The meaning of this command is..it deletes the complete directory or the file under the data directory, as we have not declared that specific variable. So we should be mindful of what we're executing.
+
+### If there is a specific variable which you don't want to declare or assaign a value ahead and you want to supply it later via CLI, you can do that with the 'export' command, but that specific value can be stored for that session only.
