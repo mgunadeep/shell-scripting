7a755d7dc0441744981cbede7689f2a1abd67ac3
guna <gunadeeprakeshm@gmail.com>
2023-07-16 15:38:27 +0530

    AutoPush

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 ### There are some variables, whose values gets changed withrespect to time. These are called as "Dynamic variables".
-### These should not be hardcoded as their data/value gets changed too often. So,instead we should softcode them using expressions to dynamically represent the results.
+### These should not be hardcoded as their data/value gets changed too often. So,instead we should softcode them using expressions to dynamically represent the result.
 
 # For example,
 echo -e "\e[32m Today's date is 22 Nov 2021 \e[0m"
@@ -8,3 +8,8 @@ echo -e "\e[32m Today's date is 22 Nov 2021 \e[0m"
 
 # So, now we'll softcode it and create an expression to dynamically query the info from system and can print the necessary data.
 echo -e "\e[36m Today's date is $(date +%D) \e[0m"
+# The expression should always be enclosed in a Paranthesis.
+
+# Like wise, we can also,dynamically see how many sessions are opened at any given moment.
+echo The opened sessions right now are $(who | wc -l)
+
