f2d891836af3cac344f5ed912f1f87cf4bdf2d18
guna <gunadeeprakeshm@gmail.com>
2023-07-16 15:51:00 +0530

    AutoPush

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -8,8 +8,8 @@ echo -e "\e[32m Today's date is 22 Nov 2021 \e[0m"
 
 # So, now we'll softcode it and create an expression to dynamically query the info from system and can print the necessary data.
 echo -e "\e[36m Today's date is $(date +%D) \e[0m"
-# The expression should always be enclosed in a Paranthesis.
+# In the above command the string inside the paranthesis is called expression. The expression should always be enclosed in a Paranthesis.
 
 # Like wise, we can also,dynamically see how many sessions are opened at any given moment.
 echo The opened sessions right now are $(who | wc -l)
-
+# In the above command we used pipe and passed the output from 1st command as input to 2nd command, where wc=wordcounts and -l option=print the newline counts.
