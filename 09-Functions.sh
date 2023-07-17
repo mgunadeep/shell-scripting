05dfb5bfa3329b18f177c5962f4f7b1b02c5646d
guna <gunadeeprakeshm@gmail.com>
2023-07-17 10:39:08 +0530

    Added functions

--- 09-Functions.sh
+++ 09-Functions.sh
@@ -9,19 +9,44 @@
 # Declaring a function 
 
 example() {
-    echo "I'm a function nmaed example"
+    echo "I'm a function named example"
     echo "I am executing the example function"
     echo " The example function is completed"
 }
+# This is how you can call a function, whose name is 'example'
+example 
 
+
+# Now, lets see another function named status.
 status() {                                                     
     echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
     echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
     echo -e "Load Average on the system from last 1 minute is $(uptime | awk -F , '{print  $3}' | awk -F : '{print $2}')"
 }
-
-# This is how you can call a function 
-example 
-
 # calling status function
 status
+
+
+
+### You can also call a function, within the function. Let's see how it can be done in below.
+
+ status() {                                                     
+    echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
+    echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
+    echo -e "Load Average on the system from last 1 minute is $(uptime | awk -F , '{print  $3}' | awk -F : '{print $2}')"
+
+echo "Calling example from within the 'status' Function"
+example
+    
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
