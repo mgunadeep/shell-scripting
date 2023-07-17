9418963ec680a87114a1df6c9375fe2bc82877ee
guna <gunadeeprakeshm@gmail.com>
2023-07-17 10:41:08 +0530

    Auto Push

--- 09-Functions.sh
+++ 09-Functions.sh
@@ -11,7 +11,7 @@
 example() {
     echo "I'm a function named example"
     echo "I am executing the example function"
-    echo " The example function is completed"
+    echo "The example function is completed"
 }
 # This is how you can call a function, whose name is 'example'
 example 
@@ -30,7 +30,7 @@ status
 
 ### You can also call a function, within the function. Let's see how it can be done in below.
 
- status() {                                                     
+status() {                                                     
     echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
     echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
     echo -e "Load Average on the system from last 1 minute is $(uptime | awk -F , '{print  $3}' | awk -F : '{print $2}')"
