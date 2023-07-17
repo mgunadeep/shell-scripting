c94d8aa94d3d5e84d403fe2aa807fbd2de9ab727
Cloud User <centos@ip-172-31-82-203.ec2.internal>
2023-07-17 05:46:41 +0000

    autopush

--- 10-trail.sh
+++
@@ -1,8 +0,0 @@
-sample() {
-    echo "I am a sample function with name sample"
-    echo "I am executing the sample function"
-    echo "Sample function is completed"
-
-    echo "Calling Status Function"
-    status
-}
