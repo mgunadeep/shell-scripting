1b994965bd68c5c5e823c6ef46ae38d8c4f53044
guna <gunadeeprakeshm@gmail.com>
2023-07-17 12:09:22 +0530

    Auto Push

--- 09-Functions.sh
+++ 09-Functions.sh
@@ -6,32 +6,33 @@
 # When to use funcions?
 # Whenever you want to use a piece of code 'n' number of times, rather than writing that n number of times, you can declare it as a "function" and can call it on need basis
 
-# Declaring a function 
+# Declaring a function is as follows,
 
-example() {
-    echo "I'm a function named example"
-    echo "I am executing the example function"
-    echo "The example function is completed"  
+### Start of the function named-'Example'
+Example() {
+    echo "I'm a function named Example"
+    echo "I'm executing this function"
+    echo "The Example function is completed"  
 }
-example    # This is how you can call a function, whose name is 'example'
+Example   ###--> This is how, you can call a function...whose name is 'Example'
+### End of the function named-'Example'
 
-
-# Now, lets see another function named status.
-status() {                                                     
-    echo -e "Good Morning and todays date is \e[32m $(date +%F)  \e[0m"
+# Now, lets see another function named-'Status'
+Status() {                                                     
+    echo -e "Good Morning and today's date is \e[32m $(date +%F)  \e[0m"
     echo -e "Number of opened sessions : \e[32m $(who | wc -l) \e[0m"    
     echo -e "Load Average on the system from last 1 minute is $(uptime | awk -F , '{print  $3}' | awk -F : '{print $2}')"
 }
-status   # calling 'status' function
+Status   ###--> Calling 'Status' function
+### End of the function named-'Status'
 
 
-
-### You can also call a function, from within the function. Let's see how it can be done.
-status () {
-    echo "calling the example from status function"
-    example
+### You can also call a function, from within a function. Let's see how it can be done.
+Status () {
+    echo "calling the Example function from within the Status function"
+    Example
 }
-status
+Status
  
  
  
