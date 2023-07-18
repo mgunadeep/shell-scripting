da057283cd531663d4baa8a7445f69ce03960c9a
guna <gunadeeprakeshm@gmail.com>
2023-07-19 01:13:43 +0530

    auto push

--- 07-read.sh
+++ 07-read.sh
@@ -2,13 +2,10 @@
 
 ### 'read' is a command, which takes the input from the user and later executes the script.
 
-echo "what is your name?"
+echo "What is your name?"
 read name
+echo "What is your age?"
+read age
 
-echo "Hai...how are you, $name!"
-sleep 1
-echo "You look really good $name"
-sleep 1
-echo "Wanna hangout tomorrow, $name"
-
+echo "Hello, $name...You are $age years old"
 ### here, in the above script the 'read' command takes the input, name from the user...and supplies it to the variable named--$name to print, whenever we called variable in the script.
