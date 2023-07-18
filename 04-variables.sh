7fc8064a6dee972397809f59ff337861f5131bc6
guna <gunadeeprakeshm@gmail.com>
2023-07-18 23:31:23 +0530

    auto push

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-### What is a variable??
-### A variable is used, to store or assaign the data and also to pass the values, later when required.
+### What is a variable?? Variable itself means-It changes, its not constant.
+### A variable is used...to store or assaign the data/value and also to pass the values.
 
 ### In other Programming languages there are different datatypes like int,float,string. But, in Bash everything is a string by default and there is no concept of datatypes here.
 
@@ -11,8 +11,8 @@
 g=7
 d=11
 echo $g
-echo ${d}
-### The above mentioned both syntaxes are also correct and can be widely accordingly.
+echo ${g}
+### The above mentioned both syntaxes are also correct and can be used accordingly.
  
 echo the respective values of the variables g and d are $g , $d 
 
@@ -27,4 +27,12 @@ echo the value of j is $j
 ### If there is a specific variable which you don't want to declare or assaign a value ahead and you want to supply it later via CLI, you can do that with the 'export' command, but that specific value can be stored for that session only. Also remember you can only supply a specific value to a variable, but you cant declare whole variables through CLI, using the export command.
 ### Here j is an undeclared variable, and we did that so to supply the value later on for that variable.
 
-### For ex:-  "rm -rf /data/${FOLDER_TO_DELETE}/ = rm -rf /data" --> The meaning of this command is..it deletes the complete directory or the file under the data directory, as we have not declared that specific variable. So we should be mindful of what we're executing.
+### Let's see an another example for a variable.
+name="Guna"
+echo "Hello $name, how are you?"
+sleep 0.5
+echo "You look really nice, $name"
+sleep 0.5
+echo "$name Wanna hangout sometime?"
+
+### For ex:-  "rm -rf /data/${FOLDER_TO_DELETE}/ = rm -rf /data" --> The meaning of this command is..it deletes the complete directory or the file under the data directory, as we have not declared that specific variable. So we should be mindfull of what we're executing.
