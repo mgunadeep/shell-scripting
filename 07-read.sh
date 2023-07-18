7c0f9cf09b01b0a64667b86dcccf9ebf48f7ac20
guna <gunadeeprakeshm@gmail.com>
2023-07-18 23:39:17 +0530

    auto push

--- 07-read.sh
+++ 07-read.sh
@@ -10,3 +10,5 @@ sleep 1
 echo "You look really good $name"
 sleep 1
 echo "Wanna hangout tomorrow, $name"
+
+### here, in the above script the 'read' command takes the input, name from the user...and supplies it to the variable named--$name to print, whenever we called variable in the script.
