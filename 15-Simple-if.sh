e2fc0cab3cb636eed2646a79ed9db0d345162e6f
guna <gunadeeprakeshm@gmail.com>
2023-07-19 15:45:45 +0530

    auto push

--- 15-Simple-if.sh
+++ 15-Simple-if.sh
@@ -3,7 +3,9 @@
 ###Let's see an exmaple of Simple-if statement,
 
 echo "What is your age?"
-read age
+    read age
+
 if [[ $age -ge 18 ]]; then
-echo "You're officially a Major"
+    echo "You're officially a Major"
+    
 fi
