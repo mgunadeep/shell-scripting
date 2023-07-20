97ad6a307dc614f95f1715d9d3a7ceaa176f1a89
guna <gunadeeprakeshm@gmail.com>
2023-07-20 11:16:48 +0530

    auto push

---
+++ 21-exit & return.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+### 'exit' command...comes out, from the execution of that script completely. And, we can also give/mention exit codes for a specific function or loop to know more about the execution status...like is it successfull or failed? So that, we can identify if there are any issues.
+### While 'return' command just comes out, from that function or that specific loop.
+
