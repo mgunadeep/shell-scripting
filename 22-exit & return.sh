f6db1e70cecb2bdac1d4123232835b4414199d8e
guna <gunadeeprakeshm@gmail.com>
2023-07-20 13:47:50 +0530

    auto push

--- 22-exit & return.sh
+++ 22-exit & return.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ### 'exit' command...comes out, from the execution of that script completely. And, we can also give/mention exit codes for a specific function or loop to know more about the execution status...like is it successfull or failed? So that, we can identify if there are any issues.
-### While 'return' command just comes out, from that function or that specific loop and will proceed with the rest of the execution,unlike exit.
+### While 'return' command just comes out, from that function and will proceed with the rest of the execution,unlike exit.
+### exit command is free to use any number from 0-255, but the values from 125+(126-255) are usually taken by the system. 
+### So, we don't use those values in the script. Hence, the script author is always recommended to use the values from (0-125).
 
-### Let's see an exampe for exit using if-else statement.
+# NOTE:- 'exit' command is different from 'exit status'.
+
+### Let's see an exampe for "exit" using conditional 'if-else' statement.
 
 ### This is a script which is going to tell you, if you can win a battle with a beast using if-else conditional statement.
 echo "The first battle with the beast starts, Get Ready..!!"
