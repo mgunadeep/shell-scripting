56ff046690fd312d60121d7595fa0ddccd5e604c
guna <gunadeeprakeshm@gmail.com>
2023-07-22 12:51:28 +0530

    auto push

--- 17-elif.sh
+++ 17-elif.sh
@@ -32,3 +32,20 @@
 # NOTE:- What is the difference between a elif and nested-if conditionals???
 # ==> In nested-if conditionals, both the parent-if and nested-if statements..should definitely has to be true, in order to-->proceed any futher or, to execute-->the next conditions in the script.
 # ==> While in the elif(else-if) conditionals, we're not saying that..both the first 'if statement' and the 'elif statement' has to be true, But..instead we are saying...if the first or above mentioned 'if statement' doesnot evaluate true, and it is false..Well, Here's an another chance...elif execute the expression with-in me, and then go for my next command.  
+
+# Let's see an example for that
+
+echo "What is your age?"
+read age
+if [ $age -le 12 ]; then
+    echo "You're a kid"
+elif [ $age -le 18 ]; then
+    echo "You're a teenager"
+elif [ $age -le 26 ]; then
+    echo "You're a grownUp"
+elif [ $age -le 59 ]; then
+    echo "You're a Matured Adult"
+
+else
+     echo "You're a senior citizen"
+fi
