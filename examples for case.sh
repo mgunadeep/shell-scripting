b73654361aa9bb751742329153f58adbdcaa821e
guna <gunadeeprakeshm@gmail.com>
2023-07-23 15:52:14 +0530

    autopush

--- examples for case.sh
+++ examples for case.sh
@@ -5,26 +5,19 @@ echo "Hello $name, Enter your preferred number from (1-7)!!"
 read number 
 
 case $number in 
-      1) echo "Sunday"
+      1) echo "Hello $name, you have selected $number and its corresponding day is Sunday"
         ;;
-      2) $number=2
-      $day = Monday
+      2) echo "Hello $name, you have selected $number and its corresponding day is Monday"
         ;;
-      3) $number=3  
-        "Tuesday"
-        ;;
-    4) $number=4
-       "Wednesday"
-        ;;
-    5) $number=5
-        "thursday"
-       ;;
-    6) $number=6
-        "friday"
-        ;;
-    7) $number=7
-       "saturday"
-        ;;
+     3) echo "Hello $name, you have selected $number and its corresponding day is Tuesday"
+     ;;
+     4)echo "Hello $name, you have selected $number and its corresponding day is Wednesday"
+     ;;
+     5) echo "Hello $name, you have selected $number and its corresponding day is Thursday"
+     ;;
+     6) echo "Hello $name, you have selected $number and its corresponding day is friday"
+     ;;
+     7) echo "Hello $name, you have selected $number and its corresponding day is Saturday"
     esac
 
     
