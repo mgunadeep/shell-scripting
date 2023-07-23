4c149eecc6a9152bb0931b5c0ce9d7be6bdfc2ca
guna <gunadeeprakeshm@gmail.com>
2023-07-23 15:30:29 +0530

    autopush

--- examples for case.sh
+++ examples for case.sh
@@ -6,26 +6,26 @@ read number
 
 case $number in 
       1) $number=1
-        echo "It's Sunday"
+        "Sunday"
         ;;
       2) $number=2
-        echo "It's Monday"
+      "Monday"
         ;;
       3) $number=3  
-        echo "Its Tuesday"
+        "Tuesday"
         ;;
     4) $number=4
-    echo "Its Wednesday"
-    ;;
+       "Wednesday"
+        ;;
     5) $number=5
-    echo "its thursday"
-    ;;
+        "thursday"
+       ;;
     6) $number=6
-    echo "its friday"
-    ;;
+        "friday"
+        ;;
     7) $number=7
-    echo "its saturday"
-    ;;
+       "saturday"
+        ;;
     esac
 
     echo "Hello $name, you have selected $number and its coresponding day is "
