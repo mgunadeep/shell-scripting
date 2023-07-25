4f27bc2d133d3b8856497a01e318eeb984734225
guna <gunadeeprakeshm@gmail.com>
2023-07-25 10:45:52 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,2 +1,41 @@
 #!/bin/bash
-echo "Hello, I am frontend Component" 
+
+webserver=nginx
+component=frontend
+logfile=/tmp/frontend.log
+
+id -u 
+if [ id -u -ne 0 ]; then 
+echo "You should be a root to perform this or should have sudo privileage"
+fi
+
+echo "Installing $webserver"
+yum install $webserver -y $ &>> $logfile
+echo $?
+if[ $? -eq 0 ]; then 
+    echo -e "\e[32m Success \0[m"
+else
+    echo -e "\e[31m failure \0[m"
+fi
+
+echo -n "Downloading the $component html content"
+curl -s -L -o /tmp/$component.zip "https://github.com/stans-robot-project/$component/archive/main.zip" &>> $logfile
+echo $? 
+
+echo -n "Deplyoing in the nginx default loccation"
+cd /usr/share/nginx/html
+rm -rf *
+
+echo -n "unzipping the content"
+unzip /tmp/$component.zip
+
+mv $component-main/* .
+mv static/* .
+rm -rf $component-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+
+
+
+
