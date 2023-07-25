de8c7362ff6ee7c7500eb90026d6e17e1ff10f74
guna <gunadeeprakeshm@gmail.com>
2023-07-25 11:13:58 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -4,7 +4,7 @@ webserver=nginx
 component=frontend
 logfile=/tmp/frontend.log
 
-ID=id -u
+ID=$(id -u)
 if [ $ID -ne 0 ]; then 
     echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
     exit 1
