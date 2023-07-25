b44c0296b1b8d1af7805cdfbdd56e059b02b14b6
guna <gunadeeprakeshm@gmail.com>
2023-07-25 13:18:16 +0530

    auto push

--- Roboshop/Components/mongodb.sh
+++ Roboshop/Components/mongodb.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-component
-echo -n "Setting up"
+component=mongodb
+logfile=/tmp/$component.log
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
+    exit 1
+fi
+
+status () {
+    if [ $1 -eq 0 ]; then 
+            echo -e "\e[32m Success \e[0m"
+    else
+            echo -e "\e[31m failure \e[0m"
+    fi
+}
+
+echo -n "Setting up the $component repo's"
+yum install -y $component-org &>> $logfile
+status $?
+
+echo -n "Enabling the $component"
+systemctl enable mongod &>> $logfile
+status $?
+
+echo -n "Starting the $component service"
+systemctl start mongod
+status $?
