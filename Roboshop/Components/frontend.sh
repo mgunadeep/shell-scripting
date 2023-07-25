12fa151d5f942681bcfd7dabbbb95247a1bab6b6
guna <gunadeeprakeshm@gmail.com>
2023-07-25 11:30:09 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -14,9 +14,9 @@ fi
 echo "Installing $webserver"
 yum install nginx -y &>> $logfile
     if[ $? -eq 0 ]; then 
-        echo -e "\e[32m Success \0[m"
+        echo -e "\e[32m Success \e0[m"
     else
-        echo -e "\e[31m failure \0[m"
+        echo -e "\e[31m failure \e0[m"
     fi
 # echo -n "Downloading the $component html content"
 # curl -s -L -o /tmp/$component.zip "https://github.com/stans-robot-project/$component/archive/main.zip" &>> $logfile
