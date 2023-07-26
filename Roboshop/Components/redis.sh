e43d5a3c8e190d88a7587ef8d3db7e8d9650efea
guna <gunadeeprakeshm@gmail.com>
2023-07-27 01:18:59 +0530

    auto push

--- Roboshop/Components/redis.sh
+++ Roboshop/Components/redis.sh
@@ -17,10 +17,29 @@ status () {
     fi
 }
 
-echo -n "Configuring the repo"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>logfile
+echo -n "Configuring the repo:"
+curl -L https://raw.githubusercontent.com/stans-robot-project/$component/main/$component.repo -o /etc/yum.repos.d/$component.repo &>>logfile
 status $?
 
-echo -n "Installing the $component"
-yum install redis - 6.2.11 -y &>>logfile
+echo -n "Installing the $component:"
+yum install $component - 6.2.11 -y &>>logfile
 status $?
+
+echo -n "Updating the bindIP:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$component.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$component/$component.conf
+status $?
+
+echo -n "Reloading the service"
+systemctl daemon-reload
+status $?
+
+echo -n "Enabling and starting the $component:"
+systemctl enable $component &>>logfile
+systemctl restart $component
+status $?
+
+echo -n "Checking the status:"
+systemctl status redis -l
+status $?
+
