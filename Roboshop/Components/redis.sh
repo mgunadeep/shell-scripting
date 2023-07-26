40d50cbc71a3c7c6d2dd4b11176572d7b101f924
guna <gunadeeprakeshm@gmail.com>
2023-07-27 01:07:17 +0530

    auto push

--- Roboshop/Components/redis.sh
+++ Roboshop/Components/redis.sh
@@ -21,18 +21,18 @@ echo -n  "Configuring the $COMPONENT repo :"
 curl -s -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
 stat $? 
 
-echo -n "Installing $COMPONENT : "
-yum install -y ${COMPONENT}-6.2.11   &>> $LOGFILE
-stat $? 
+# echo -n "Installing $COMPONENT : "
+# yum install -y ${COMPONENT}-6.2.11   &>> $LOGFILE
+# stat $? 
 
-echo -n "Enabling the DB visibility :"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}/${COMPONENT}.conf
-stat $? 
+# echo -n "Enabling the DB visibility :"
+# sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
+# sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}/${COMPONENT}.conf
+# stat $? 
 
-echo -n "Starting $COMPONENT : "
-systemctl daemon-reload $COMPONENT      &>> $LOGFILE
-systemctl enable $COMPONENT      &>> $LOGFILE
-systemctl restart $COMPONENT       &>> $LOGFILE
-stat $?
+# echo -n "Starting $COMPONENT : "
+# systemctl daemon-reload $COMPONENT      &>> $LOGFILE
+# systemctl enable $COMPONENT      &>> $LOGFILE
+# systemctl restart $COMPONENT       &>> $LOGFILE
+# stat $?
 
