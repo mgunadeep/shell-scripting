114e34bd4706018febc40ac529a581e1b5467e38
guna <gunadeeprakeshm@gmail.com>
2023-07-27 01:22:34 +0530

    auto push

--- Roboshop/Components/redis.sh
+++ Roboshop/Components/redis.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
 component=redis
-logfile=/tmp/$component.log
-
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
-    exit 1
-fi
-
-status () {
-    if [ $1 -eq 0 ]; then 
-            echo -e "\e[32m Success \e[0m"
-    else
-            echo -e "\e[31m failure \e[0m"
-    fi
-}
+source components/common.sh
 
 echo -n "Configuring the repo:"
 curl -L https://raw.githubusercontent.com/stans-robot-project/$component/main/$component.repo -o /etc/yum.repos.d/$component.repo &>>logfile
