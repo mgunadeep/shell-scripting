5d9d3ba11e34587ffbe2c0cdf4d5c234b125dfdd
guna <gunadeeprakeshm@gmail.com>
2023-07-26 11:19:26 +0530

    auto push

--- Roboshop/Components/mongodb.sh
+++ Roboshop/Components/mongodb.sh
@@ -25,6 +25,14 @@ echo -n "Installing the $component..:"
 yum install -y $component-org &>> $logfile
 status $?
 
+echo -n "Updating the IP and giving DB access to other internal components:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status $?
+
+echo -n "Restarting the $component service...:"
+systemctl daemon-reload mongod
+status $?
+
 echo -n "Enabling the $component:"
 systemctl enable mongod &>> $logfile
 status $?
@@ -33,15 +41,6 @@ echo -n "Starting the $component service..:"
 systemctl start mongod
 status $?
 
-echo -n "Updating the IP and giving DB access to other internal components:"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-status $?
-
-echo -n "Restarting the $component service...:"
-systemctl daemon-reload mongod
-systemctl enable mongod  &>> $logfile
-systemctl restart mongod 
-status $?
 
 echo -n "Downloading the $component schema:"
 curl -s -L -o /tmp/$component.zip "https://github.com/stans-robot-project/$component/archive/main.zip" &>> $logfile
@@ -52,7 +51,7 @@ cd /tmp
 unzip -o $component.zip  &>> $logfile
 status $?
 
-echo -n "Injecting the schema.."
+echo -n "Injecting the schema..:"
 cd $component-main
 mongo < catalogue.js &>> $logfile
 mongo < users.js     &>> $logfile
