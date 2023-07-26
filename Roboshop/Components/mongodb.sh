0bd79cc9ba63b8225c7273e05419e9645823a331
guna <gunadeeprakeshm@gmail.com>
2023-07-27 00:09:07 +0530

    auto push

--- Roboshop/Components/mongodb.sh
+++ Roboshop/Components/mongodb.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 component=mongodb
-logfile=/tmp/$component.log
-
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m You should be a "root" to perform this action or should obtain, "sudo" privileages \e[0m"
-    exit 1
-fi
-
-status () {
-    if [ $1 -eq 0 ]; then 
-            echo -e "\e[32m Success \e[0m"
-    else
-            echo -e "\e[31m failure \e[0m"
-            exit 2
-    fi
-}
+source components/common.sh
 
 echo -n "Setting up the $component repo's:"
 curl -s -o /etc/yum.repos.d/$component.repo https://raw.githubusercontent.com/stans-robot-project/$component/main/mongo.repo &>> $logfile
