68346d924f0af30eab91f47edd457119d52e6387
guna <gunadeeprakeshm@gmail.com>
2023-07-27 01:00:21 +0530

    auto push

--- Roboshop/Components/redis.sh
+++ Roboshop/Components/redis.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-component=redis
+COMPONENT=redis
 source components/common.sh
 
-echo -n "Configuring the repo:"
-curl -L https://raw.githubusercontent.com/stans-robot-project/$component/main/$component.repo -o /etc/yum.repos.d/$component.repo  &>> $logfile
-status $?
+echo -n  "Configuring the $COMPONENT repo :"
+curl -s -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo -o /etc/yum.repos.d/${COMPONENT}.repo
+stat $? 
 
-echo -n "Installing $component:"
-yum install redis-6.2.11 -y &>> $logfile
-status $?
+echo -n "Installing $COMPONENT : "
+yum install -y ${COMPONENT}-6.2.11   &>> $LOGFILE
+stat $? 
 
-echo -n "Updating the bindIP address:"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$component.conf &>> $logfile
-status $?
+echo -n "Enabling the DB visibility :"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/${COMPONENT}/${COMPONENT}.conf
+stat $? 
 
-echo -n "Starting the $component:"
-systemctl daemon-reload
-systemctl enable $component &>> $logfile
-systemctl start $component
-status $?
+echo -n "Starting $COMPONENT : "
+systemctl daemon-reload $COMPONENT      &>> $LOGFILE
+systemctl enable $COMPONENT      &>> $LOGFILE
+systemctl restart $COMPONENT       &>> $LOGFILE
+stat $?
 
-echo -n "Checking the Redis status:"
-systemctl status $component -l
-status $?
