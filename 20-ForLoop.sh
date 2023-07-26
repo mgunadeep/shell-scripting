11546278d311782058c5d72cb066bd547a1f400a
guna <gunadeeprakeshm@gmail.com>
2023-07-26 21:51:02 +0530

    auto push

--- 20-ForLoop.sh
+++ 20-ForLoop.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+for variable in list;
+do
+    # commands to be executed for each value in the list
+done
 
-## x becomes every number in each iteration in the given list of 1 to 5.
-# for x in 1,2,3,4,5
+
+## variable "x" becomes every number in each iteration in the given "list" of "1 to 5".
+# for x in 1,2,3,4,5;
 # do 
 #     echo $x
 # done
 
-# Below, is how you declare a range, 
-for x in {1..10};   ## ==>> For variable-"x" in the "list" {1 to 10}
+# Example..
+for x in {1..10};  #==> This is how, you declare a range.
 do 
     echo $x
 done
 
-# for x in $(cat cities.txt)
+# for x in $(cat cities.txt);  #==> This is how, to give input from a file.
 # do 
 #     echo $x
 # done
 
 ## 'Break' is to interrupt/stop the loop.
-## 'Continue' means...that it will literally, ignore/skip that specific loop given for x 
+## 'Continue' means, it will literally, ignore/skip that specific loop given for x.
