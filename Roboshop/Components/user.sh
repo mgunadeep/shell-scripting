85ff3245e9f08477ed0b76619928f0450b05a22e
guna <gunadeeprakeshm@gmail.com>
2023-07-27 11:45:17 +0530

    auto push

--- Roboshop/Components/user.sh
+++ Roboshop/Components/user.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 component=user
-source components/common.sh
+logfile=/tmp/$component.log
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
+    exit 1
+fi
+
+status () {
+    if [ $1 -eq 0 ]; then 
+            echo -e "\e[32m Success \e[0m"
+    else
+            echo -e "\e[31m failure \e[0m"
+    fi
+}
 
 echo -n "Configuring the repo:"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $logfile
