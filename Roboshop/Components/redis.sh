af0d39eed924c8fde080e1233c481de39278022d
guna <gunadeeprakeshm@gmail.com>
2023-07-28 20:05:16 +0530

    auto push

--- Roboshop/Components/redis.sh
+++ Roboshop/Components/redis.sh
@@ -22,16 +22,12 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/$component/main/$c
 status $?
 
 echo -n "Installing the $component:"
-yum install $component - 6.2.11 -y &>>logfile
+yum install $component- 6.2.11 -y &>>logfile
 status $?
 
 echo -n "Updating the bindIP:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$component.conf
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/$component/$component.conf
-status $?
-
-echo -n "Reloading the service"
-systemctl daemon-reload
 status $?
 
 echo -n "Enabling and starting the $component:"
