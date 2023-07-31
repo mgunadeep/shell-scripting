4b9d2015f4522e14e2b79362970fa3a16444fe79
guna <gunadeeprakeshm@gmail.com>
2023-07-31 12:14:35 +0530

    auto push

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -18,14 +18,14 @@ status () {
 
 echo -n "Configuring the repo:"
 curl -s -L -o /etc/yum.repos.d/$component.repo https://raw.githubusercontent.com/stans-robot-project/$component/main/$component.repo &>> $logfile
-status $1
+status $?
 
 echo -n "Installing the $component:"
 yum install $component-community-server -y &>> $logfile
-status $1
+status $?
 
 echo -n "Starting the $component:"
 systemctl enable mysqld &>> $logfile
 systemctl start mysqld
-status $1
+status $?
 
