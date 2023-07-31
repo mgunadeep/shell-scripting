add18427ba4833a121f984149a3d75c8623ac3a8
guna <gunadeeprakeshm@gmail.com>
2023-07-31 12:16:32 +0530

    auto push

--- Roboshop/Components/mysql.sh
+++ Roboshop/Components/mysql.sh
@@ -2,19 +2,7 @@
 component=mysql
 logfile=/tmp/$component.log
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
-    exit 1
-fi
-
-status () {
-    if [ $1 -eq 0 ]; then 
-            echo -e "\e[32m Success \e[0m"
-    else
-            echo -e "\e[31m failure \e[0m"
-    fi
-}
+source components/common.sh
 
 echo -n "Configuring the repo:"
 curl -s -L -o /etc/yum.repos.d/$component.repo https://raw.githubusercontent.com/stans-robot-project/$component/main/$component.repo &>> $logfile
