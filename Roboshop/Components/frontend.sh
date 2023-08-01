e6c1e83ebd58954d05310996c05e536d1e49af95
guna <gunadeeprakeshm@gmail.com>
2023-08-01 13:06:25 +0530

    auto push

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -62,7 +62,7 @@ systemctl restart $webserver
 status $?
 
 echo -n "Updating the Backend DNS in reverseproxy file:"
-for component in catalogue user cart ;
+for component in catalogue user cart shipping;
 do 
     sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
 done
