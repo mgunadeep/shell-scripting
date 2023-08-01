272fe957c9a635b12196dd96e226582999932459
guna <gunadeeprakeshm@gmail.com>
2023-08-01 14:22:24 +0530

    auto push

--- Roboshop/Components/rabbitmq.sh
+++ Roboshop/Components/rabbitmq.sh
@@ -40,7 +40,7 @@ systemctl status $component-server -l  &>> $logfile
 status $?
 
 
-id $appuser &>> $logfile
+rabbitmqctl list_users | grep roboshop &>> $logfile
 if [ $? -ne 0 ]; then              ### This function is written-to take care of exception handling. So, if the service accnt is already created it doesn't do anything. But, if the service account is not created, then it creates a new one.
    echo -n "Creating a $appuser service account:"
    rabbitmqctl add_user roboshop roboshop123  &>> $logfile
