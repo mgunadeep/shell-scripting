15540354ca81c0bd99331043ca75112904f435c6
guna <gunadeeprakeshm@gmail.com>
2023-08-01 13:44:45 +0530

    auto push

---
+++ Roboshop/Components/rabbitmq.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+
+appuser=roboshop
+component=rabbitmq
+logfile=/tmp/$component.log
+
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m You should be a root to perform this or should have sudo privileage \e[0m"
+    exit 1
+fi
+
+status () {
+    if [ $1 -eq 0 ]; then 
+            echo -e "\e[32m Success \e[0m"
+    else
+            echo -e "\e[31m failure \e[0m"
+    fi
+}
+
+echo -n "Downloading the Erlang dependency:"
+curl -s https://packagecloud.io/install/repositories/$component/erlang/script.rpm.sh | sudo bash &>> $logfile
+status $?
+
+echo -n "Configuring the repo:"
+curl -s https://packagecloud.io/install/repositories/$component/rabbitmq-server/script.rpm.sh | sudo bash  &>> $logfile
+status $?
+
+echo -n "Installing the $component:"
+yum install rabbitmq-server -y &>> $logfile
+status $?
+
+echo -n "Enabling and restarting the $component:"
+systemctl enable $component-server  &>> $logfile
+systemctl restart $component-server
+status $?
+
+echo -n "Checking the status of $component:"
+systemctl status $component-server -l
+status $?
