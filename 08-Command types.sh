9e0568fe9e7d7899a6efa13598b7d8362c6264b1
mgunadeep <81462289+mgunadeep@users.noreply.github.com>
2025-03-21 18:29:25 +0530

    Update 08-Command types.sh

--- 08-Command types.sh
+++ 08-Command types.sh
@@ -4,5 +4,7 @@
 
 # 1) Binary                   ( /bin , /sbin )
 # 2) Aliases                  ( Alises are shortcuts,  alias net="netstat -tulpn" )
+# --> But, these aliases are temporary in nature i.e they would be gone after you signoff. So, to make them permanent we have to add these aliases in to bash_profile.
+#     In linux, every user has their own shell profile in their(user's) home directory called as ~/.bash_profile. You have to add these aliases in that profile to make these permanent.
 # 3) Shell Built-in Commands  
 # 4) Functions                # Functions are nothing but a set of command that can be written in a sequence and can be called 'n' number of times as per your choice.
